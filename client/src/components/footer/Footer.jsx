20fa03c55ba66b118be4977c37efd7a711d9e07e
Akash Raj <akashsingh.77312@gmail.com>
2024-04-17 01:11:20 +0530

    add event page

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Footer.css'
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">@2022 Privacy Policy Terms & Condition</div>
+    </>
+  );
+};
+
+export default Footer;
